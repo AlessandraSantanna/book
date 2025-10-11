21321ef19eaf539dc1defc3e4a3baa0b7299fd13
Alessandra Santanna <alesy2007@gmail.com>
2025-10-10 22:21:02 -0300

    responsivo do login

--- src/app/landing1/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Teste() {
-  return <h1>Landing está funcionando ✅</h1>;
-}
-
--- src/backend/db.js
+++ src/backend/db.js
@@ -1,6 +1,32 @@
-import mysql from "mysql2/promise";
 
-export const pool = mysql.createPool({
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = await mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+});
+
+export default connection;
+
+
+
+
+
+
+/*import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql.createPool(process.env.DATABASE_URL + "?ssl={" + 
+  JSON.stringify({ rejectUnauthorized: true }) + 
+"}");
+
+export default pool;
+
+/*export const pool = mysql.createPool({
   host: "localhost",
   user: "root",        // seu usuário MySQL
   password: "123456", // sua senha
@@ -10,4 +36,4 @@ export const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-console.log("✅ Conexão com o MySQL configurada (usando pool).");
+console.log("✅ Conexão com o MySQL configurada (usando pool).");*/
