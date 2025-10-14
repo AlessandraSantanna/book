0c7ef04ce1b49fd4fda349a1c57fa87edef3735f
Alessandra Santanna <alesy2007@gmail.com>
2025-10-14 20:23:57 -0300

    server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,7 +6,7 @@ import livrosRouter from "./routes/livros.js";
 const app = express();
 
 // 🧠 Declare a porta primeiro:
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +14,11 @@ app.use("/api/livros", livrosRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  
+  app.get("/", (req, res) => {
+  res.send("✅ API do Book está online!");
+});
+
 });
 
 
